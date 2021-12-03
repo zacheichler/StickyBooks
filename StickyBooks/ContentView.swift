1af686cbe06bdf2a74d6f47d2ad7deb7c2fd3568
zach <zacheichler@gmail.com>
2021-12-02 22:24:49 -0500

    Initial Commit

---
+++ StickyBooks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StickyBooks
+//
+//  Created by Zach Eichler on 2021-12-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StickyBooks/StickyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyBooksApp.swift
+//  StickyBooks
+//
+//  Created by Zach Eichler on 2021-12-02.
+//
+
+import SwiftUI
+
+@main
+struct StickyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
